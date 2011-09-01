ea72a6e6fa64633f9a335682e24188f65346b01f
RusselYang <russelyang@gmail.com>
2011-08-31 23:45:26 -0700

    2nd test

--- test/chat_room_controller_test.js
+++ test/chat_room_controller_test.js
@@ -1,7 +1,18 @@
+var chatRoomController = require("../lib/chat_room_controller");
+
 exports["chartRoomController should be object"] = function(test) {
-	var chatRoomController = require("../lib/chat_room_controller");
         test.ok(chatRoomController != null);
         test.ok(typeof chatRoomController.create == "function");
         test.done();
 };
+
+exports["chatRoomController.create should return a object with request and reponse"] = function(test) {
+	var req = {};
+	var res = {};
+	
+	var controller = chatRoomController.create(req, res);
+	test.strictEqual(req, controller.request);
+	test.strictEqual(res, controller.response);
+	test.done();
+}
 
